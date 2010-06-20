7aa1be17c3875c4b5178709ae596bb2921251872
snow <snow@swganh.org>
2010-06-20 11:26:57 -0400

    Initial Commit  - Base Website look & feel

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <title>SWG:ANH &bull; Bringing PreCU back to life...</title>
+
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link rel="stylesheet" type="text/css" media="screen" href="theme/main.css" />
+  </head>
+  <body id="page">
+    <div id="page-section-main">
+      <div id="page-section-header" class="site-section">
+        <div id="page-section-logo">
+          <a href="/"><img src="theme/main/header.jpg" width="850" height="150" border="0" /></a>
+        </div>
+        
+        <div id="page-section-mainmenu">
+          <ul>
+            <li><a href=""><span>menu00</span></a></li>
+            <li><a href=""><span>menu01</span></a></li>
+            <li><a href=""><span>menu02</span></a></li>
+            <li><a href=""><span>menu03</span></a></li>
+            <li><a href=""><span>menu04</span></a></li>
+            <li><a href=""><span>menu05</span></a></li>
+            <li><a href=""><span>menu06</span></a></li>
+            <li><a href=""><span>menu07</span></a></li>
+            <li><a href=""><span>menu08</span></a></li>
+            <li><a href=""><span>login</span></a></li>
+          </ul>
+        </div>
+      </div>
+        
+      <div id="page-section-body" class="site-section">
+        <div id="page-section-content">
+          <!--// PAGE CONTENT STARTS HERE -->
+          
+          
+          <h2>SWG:ANH .com Design Template</h2>
+          <h3>Nearly Complete... css file could do with some formatting</h3>
+          
+          <p>This is the basic page layout for the swganh.com website</p>
+          
+          
+          <!--// PAGE CONTENT ENDS HERE -->
+        </div>
+        <div class="clear"></div>
+      </div>
+    </div>
+    
+    <div id="page-section-footer" class="site-section">   
+      <div id="copyright">
+        &copy; 2006, 2010 <a href="/">SWG:ANH</a> 
+        &bull; Designed by <a href="/">SWG:ANH</a>
+      </div>
+    </div>
+  </body>
+</html>
